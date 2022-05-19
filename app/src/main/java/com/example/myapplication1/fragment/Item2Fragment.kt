30585c3f65f1006546e4d9c402648737191765dc
Пахомов Даниил <pda@pozhavt.ru>
2022-05-19 11:13:46 +0300

    1 Session

--- app/src/main/java/com/example/myapplication1/fragment/Item2Fragment.kt
+++ app/src/main/java/com/example/myapplication1/fragment/Item2Fragment.kt
@@ -1,25 +1,42 @@
 package com.example.myapplication1.fragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.INFO
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.myapplication1.R
+import com.example.myapplication1.presentation.SingIn
+import kotlinx.android.synthetic.main.fragment_item2.*
+import java.util.logging.Level.INFO
 
 class Item2Fragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item2, container, false)
+        var view = inflater.inflate(R.layout.fragment_item2, container, false)
+        view.findViewById<Button>(R.id.sing_in).setOnClickListener {
+            var intent = Intent(this.context,SingIn::class.java);
+            startActivity(intent);
+        }
+        return view
     }
 
+    override fun onAttach(context: Context) {
+
+        super.onAttach(context)
+    }
 
 }
--- app/src/main/java/com/example/myapplication1/presentation/OnBoardingScreen.kt
+++ app/src/main/java/com/example/myapplication1/presentation/OnBoardingScreen.kt
@@ -6,11 +6,13 @@ import com.example.myapplication1.R
 import com.example.myapplication1.adapter.ViewPagerAdapter
 import com.example.myapplication1.fragment.Item1Fragment
 import kotlinx.android.synthetic.main.activity_on_boarding_screen.*
+import kotlinx.android.synthetic.main.fragment_item2.*
 
 class OnBoardingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_screen)
         ViewPage.adapter = ViewPagerAdapter(this);
+
     }
 }
